f3935b7eb5bb84fd6e653cd0f1a25c3e30058171
moacirdavidag <moacirdavidag@gmail.com>
2023-12-14 13:26:32 -0300

    view: Header e Footer implementados
    Versão inicial dos componentes implementada

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -7,11 +7,42 @@
     <title>DevShop - @yield('titulo')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body class="container-fluid">
+<body class="container-fluid p-0 w-100">
     @section('header')
-        <h1>DevShop - @yield('titulo')</h1>
+        <header class="container-fluid p-0">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary p-3" data-bs-theme="dark">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="/">DevShop</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+        </header>
     @show
 
     @yield('conteudo')
+
+    @section('footer')
+        <footer class="container-fluid w-100 p-3 bg-body-tertiary">
+            <p>DevShop - Moacir David 2023</p>
+        </footer>
+    @show
 </body>
 </html>
