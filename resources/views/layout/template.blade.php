0425a18bf8a56a3fdf35450df274ecc0053a52b8
moacirdavidag <moacirdavidag@gmail.com>
2023-12-14 13:08:23 -0300

    view: Testando configurações de views com o Blade

---
+++ resources/views/layout/template.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DevShop - @yield('titulo')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body class="container-fluid">
+    @section('header')
+        <h1>DevShop - @yield('titulo')</h1>
+    @show
+
+    @yield('conteudo')
+</body>
+</html>
---
+++ resources/views/produtos/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.template')
+@section('titulo', 'Produtos')
+
+@section('conteudo')
+    <h1>Produtos</h1>
+@endsection
